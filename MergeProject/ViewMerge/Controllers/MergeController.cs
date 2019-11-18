9a388cbe0ae1364b7cba8c8f05c4b30f472abdfd
Alex Sasov <sasov1993@mail.ru>
2019-11-18 22:22:34 +0300

    добавил простой проект для объединения и тестирования view, jquery-validation

---
+++ MergeProject/ViewMerge/Controllers/MergeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewMerge.Models;
+
+namespace ViewMerge.Controllers
+{
+    public class MergeController : Controller
+    {
+        List<UserView> list = new List<UserView>();
+
+        private void GetData()
+        {
+            for(int i = 1; i < 10; i++)
+            {
+                list.Add(FactoryUserView.Create(i));
+            }
+        }
+
+        public IActionResult Index()
+        {
+            GetData();
+            return View(list);
+        }
+    }
+}
---
+++ MergeProject/ViewMerge/Models/UserView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewMerge.Models
+{
+    public class UserView
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string BirthdayStr {
+            get
+            {
+                return Birthday.ToString("dd.MM.yyyy");
+            }
+        }
+        public string WorkPlace { get; set; }
+        public string WorkPosition { get; set; }
+        public string HomeAddress { get; set; }
+    }
+
+    public static class FactoryUserView
+    {
+        public static UserView Create(int i)
+        {
+            return new UserView()
+            {
+                Id = i,
+                LastName = $"lastName {i}",
+                FirstName = $"firstName {i}",
+                MiddleName = $"middleName {i}",
+                Birthday = DateTime.Today.AddDays(-i * 30),
+                WorkPosition = $"workPosition {i}",
+                WorkPlace = $"workPlace {i}",
+                HomeAddress = $"homeAddress {i}"
+            };
+        }
+    }
+}
