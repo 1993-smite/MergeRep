9f26841626b72105904cb0d03704d3301570a3a7
Alex Sasov <sasov1993@mail.ru>
2019-11-25 23:46:06 +0300

    добавил класс для отслеживания изменений модели

---
+++ MergeProject/ViewMerge/wwwroot/js/observer-monitor.js
@@ -0,0 +1,38 @@
+﻿class Observer {
+
+    constructor(target, config, callback) {
+        // Выбираем целевой элемент
+        this.target = target;
+
+        // Конфигурация observer (за какими изменениями наблюдать)
+        this.config = config || {
+            attributes: true,
+            childList: true,
+            subtree: true
+        };
+
+        // Функция обратного вызова при срабатывании мутации
+        this.callback = callback || function (mutationsList, observerEl) {
+            for (let mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    console.log('A child node has been added or removed.');
+                } else if (mutation.type === 'attributes') {
+                    console.log('The ' + mutation.attributeName + ' attribute was modified.');
+                }
+            }
+        };
+
+        // Создаем экземпляр наблюдателя с указанной функцией обратного вызова
+        this.observer = new MutationObserver(this.callback);
+
+        // Начинаем наблюдение за настроенными изменениями целевого элемента
+        this.observer.observe(this.target, this.config);
+
+        return this;
+    }
+
+    stopObserver() {
+        // Позже можно остановить наблюдение
+        this.observer.disconnect();
+    }
+}
