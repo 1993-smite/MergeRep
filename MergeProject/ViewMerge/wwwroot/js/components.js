2b3342498c8fe81ba85216f0c3e2d376c2fc8430
Alex Sasov <sasov1993@mail.ru>
2019-12-05 22:29:22 +0300

    добавил парочку компонентов и либу для их написания

---
+++ MergeProject/ViewMerge/wwwroot/js/components.js
@@ -0,0 +1,69 @@
+﻿/************************** lazy-input-text-change **********************************************/
+jBlocks.define('lazy-input-text-change',
+{
+    events: {
+        'keyup': 'update'
+    },
+
+    methods: {
+        oninit: function () {
+            this._currentValue = this.props.initialValue || "";
+            this._timeOut = Number(this.props.timeOut) || 1000;
+            this._blockUpdate = false;
+        },
+        ondestroy: function () {
+            this._currentValue = null;
+            this._timeOut = null;
+            this._blockUpdate = null;
+        },
+        /**
+            * Increases the counter, emits changed event
+            */
+        update: function () {
+            if (!this._blockUpdate) {
+                this._blockUpdate = true;
+                var component = this;
+                setTimeout(function () {
+                        component.emit('changed',
+                            {
+                                value: this._currentValue
+                            });
+                        component._blockUpdate = false;
+                    },
+                    this._timeOut);
+            }
+        }
+    }
+});
+/***************************************************************************************/
+jBlocks.define('input-change',
+{
+    events: {
+        'focus': 'check',
+    },
+
+    methods: {
+        oninit: function () {
+            this._lastValue = null;
+            this._curValue = null;
+            var element = this;
+            this.node.onchange = function () {
+                console.log("change", element);
+                element._lastValue = element._curValue;
+                element._curValue = element.node.value;
+                element.emit('change', { Val: element.node.value, lastVal: element._lastValue, el: element });
+            }
+        },
+        ondestroy: function () {
+            this._lastValue = null;
+            this._curValue = null;
+        },
+        /**
+            * Increases the counter, emits changed event
+            */
+        check: function () {
+            this._lastValue = this.node.value;
+            this.emit('focus', { element: this });
+        }
+    }
+});
