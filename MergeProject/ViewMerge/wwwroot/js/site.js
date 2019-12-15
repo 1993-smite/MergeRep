cb397361f7de0645ce2e84dedcfe381274ab64f8
Alex Sasov <sasov1993@mail.ru>
2019-12-15 17:01:44 +0300

    добавил scss file перестраиваем стили на динамику

--- MergeProject/ViewMerge/wwwroot/js/site.js
+++ MergeProject/ViewMerge/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    $(".input-group-append .btn-close").click(function () {
+        $("#search-country").val("");
+    });
+    $(".glyphicon-search", ".input-group-prepend ").click(function () {
+        console.log($('#search-country').fias().change());
+    });
+});
