4490005d8916ad5e7133a542b99460820b3d3270
Alex Sasov <sasov1993@mail.ru>
2019-12-22 13:00:37 +0300

    добавил svg картинку и внес интерактивности

---
+++ MergeProject/ViewMerge/Controllers/SVGController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewTestProject.Controllers
+{
+    public class Point
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+    public static class PointFactory
+    {
+        public static Point CreatePoint(int index)
+        {
+            return new Point
+            {
+                Id = index,
+                Title = $"Title №{index}"
+            };
+        }
+    }
+
+    public class SVGController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            var points = new List<Point>();
+            for(int i = 0; i < 20; i++)
+            {
+                points.Add(PointFactory.CreatePoint(i));
+            }
+
+            return View(points);
+        }
+    }
+}
