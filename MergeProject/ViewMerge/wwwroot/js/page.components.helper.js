47a44e8a6572acafa501950059dff9ff71aed4ab
Alex Sasov <sasov1993@mail.ru>
2020-01-11 23:06:39 +0300

    добавил vue component

---
+++ MergeProject/ViewMerge/wwwroot/js/page.components.helper.js
@@ -0,0 +1,11 @@
+﻿Vue.component('form-tr-input', {
+    // camelCase в JavaScript
+    props: ['id', 'label', 'name', 'value', 'autocomplite'],
+    template: `<div class="input-group mb-3">
+                    <label class= "col-md-2" :for="id">{{label}}</label> 
+                    <input class="col-md-4 form-control" :id="id" :name="name"
+                        type="text" :list="autocomplite"
+                        v-bind:value="value" v-on:input="$emit('input', $event.target.value)" />
+                    <slot></slot>
+               </div>`
+});
