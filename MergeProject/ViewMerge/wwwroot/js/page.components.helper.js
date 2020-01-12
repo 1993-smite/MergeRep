b4ac83549c395a61542c9952ec0a349f73cc16ec
Alex Sasov <sasov1993@mail.ru>
2020-01-12 20:32:39 +0300

    компонент сделал более гибким и перекинул всю форму на него

--- MergeProject/ViewMerge/wwwroot/js/page.components.helper.js
+++ MergeProject/ViewMerge/wwwroot/js/page.components.helper.js
@@ -1,10 +1,16 @@
 ﻿Vue.component('form-tr-input', {
     // camelCase в JavaScript
-    props: ['id', 'label', 'name', 'value', 'autocomplite'],
+    props: ['id', 'label', 'name', 'value', 'autocomplite', 'type'],
+    filters: {
+        defType: function (val) {
+            if (val) return val;
+            else return "text";
+        }
+    },
     template: `<div class="input-group mb-3">
                     <label class= "col-md-2" :for="id">{{label}}</label> 
                     <input class="col-md-4 form-control" :id="id" :name="name"
-                        type="text" :list="autocomplite"
+                        :type="type | defType" :list="autocomplite"
                         v-bind:value="value" v-on:input="$emit('input', $event.target.value)" />
                     <slot></slot>
                </div>`
