f9d87f2a0111a5e7c16dd6afe78dc961c9273c09
Alex Sasov <sasov1993@mail.ru>
2020-02-16 21:39:35 +0300

    тесты с хелперами

---
+++ MergeProject/ViewMerge/TagHelpers/TimerTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace TagHelpersApp.TagHelpers
+{
+    public class TimerTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";    // заменяет тег <timer> тегом <div>
+                                       // устанавливаем содержимое элемента
+            output.Content.SetContent($"Текущее время: {DateTime.Now.ToString("HH:mm:ss")}");
+        }
+    }
+}
