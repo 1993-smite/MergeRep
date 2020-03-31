9a289783cac135085ea2e79fbbd62a1bfa16e26f
Alex Sasov <sasov1993@mail.ru>
2020-03-31 22:27:15 +0300

    добавил тег-хелпер для отображения логина пользователя

---
+++ MergeProject/WebVueTest/Helpers/HtmlViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using WebVueTest.Models;
+
+namespace ViewComponentsApp.Components
+{
+    [ViewComponent]
+    public class User: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            string login = HttpContext.Request.Cookies[appUser.sessionKey];
+            return new ContentViewComponentResult(login); ;
+        }
+    }
+}
