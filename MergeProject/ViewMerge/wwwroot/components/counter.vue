27df30fd535e88ee254b4b034fa06f9bb598d462
Alex Sasov <sasov1993@mail.ru>
2020-04-04 20:27:12 +0300

    удалил лишнее

--- MergeProject/ViewMerge/wwwroot/components/counter.vue
+++
@@ -1,8 +0,0 @@
-﻿Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">Счётчик кликов — {{ count }}</button>'
-})
