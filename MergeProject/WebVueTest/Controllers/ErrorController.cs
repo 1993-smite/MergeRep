3d7ca1d95fb04a121118e23f651c3b344bdcd67e
Alex Sasov <sasov1993@mail.ru>
2020-06-05 21:29:36 +0300

    добавил страницу с ошибками

---
+++ MergeProject/WebVueTest/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebVueTest.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Erroroller
+        public ActionResult Error(Exception exception)
+        {
+            return View(exception);
+        }
+    }
+}
