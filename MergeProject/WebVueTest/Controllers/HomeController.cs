ea24d5cb12e5d927acf541aac52455a39d3beb91
Alex Sasov <sasov1993@mail.ru>
2020-06-20 22:31:45 +0300

    убрал лишние вьюхи

--- MergeProject/WebVueTest/Controllers/HomeController.cs
+++ MergeProject/WebVueTest/Controllers/HomeController.cs
@@ -23,29 +23,9 @@ namespace WebVueTest.Controllers
             //return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
+        public IActionResult Html()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
