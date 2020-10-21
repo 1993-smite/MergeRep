83407ba98ed70bd40eca793e40c105ca0e5da2a1
Alex Sasov <sasov1993@mail.ru>
2020-10-21 22:27:22 +0300

    добавил проект с апи

---
+++ MergeProject/WebAPI/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET api/values/5
+        /// <summary>
+        /// get for user by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public User Get(int id)
+        {
+            var usr = new User(id);
+
+            return usr;
+        }
+
+        // POST api/values
+        /// <summary>
+        /// пост для юзера
+        /// </summary>
+        /// <param name="user"></param>
+        public void Post([FromBody]User user)
+        {
+
+        }
+
+    }
+}
---
+++ MergeProject/WebAPI/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class User
+    {
+        public int Id { private set; get; }
+        public string LastName { private set; get; }
+        public string Name { private set; get; }
+        public string MiddleName { private set; get; }
+
+        public User(int id)
+        {
+            Id = id;
+            LastName = $"Last Name {Id}";
+            Name = $"Name {Id}";
+            MiddleName = $"Middle Name {Id}";
+        }
+
+        /// <summary>
+        /// ФИО для юзера
+        /// </summary>
+        public string FIO
+        {
+            get
+            {
+                return $"{LastName} {Name} {MiddleName}";
+            }
+        }
+    }
+}
