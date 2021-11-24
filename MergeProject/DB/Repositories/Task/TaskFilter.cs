8c35d31895d542818c6210ec5c1bb38ca70477f2
Alex Sasov <sasov1993@mail.ru>
2021-11-24 21:23:52 +0300

    task repository

---
+++ MergeProject/DB/Repositories/Task/TaskFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB.Repositories.Task
+{
+    public class TaskFilter: Filter
+    {
+        public enum State
+        {
+            Active=1,
+            Dalete=9
+        }
+
+        public int Id;
+    }
+}
---
+++ MergeProject/DB/Repositories/Task/TaskRepository.cs
@@ -0,0 +1,82 @@
+﻿using DB.DBModels;
+using PostgresApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DB.Repositories.Task
+{
+    public class TaskRepository: CommonRepository<DBTask, TaskFilter>
+    {
+        /// <summary>
+        /// get tasks
+        /// </summary>
+        /// <returns></returns>
+        public override IEnumerable<DBTask> GetList([NotNull] TaskFilter filter)
+        {
+            var tasks = new List<DBTask>();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                tasks = db.DBTasks
+                    .Where(x => x.Status == (int)TaskFilter.State.Active)
+                    .OrderBy(x => x.Date)
+                    .ToList();
+            }
+            return tasks;
+        }
+
+        /// <summary>
+        /// get db task bi id
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public override DBTask Get([NotNull]TaskFilter filter)
+        {
+            var task = new DBTask();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                task = db.DBTasks
+                    .Where(x => x.Id == filter.Id)
+                    .AsNoTracking()
+                    .FirstOrDefault();
+            }
+            return task;
+        }
+
+        /// <summary>
+        /// save task 
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        public DBTask Save(ApplicationContext db, DBTask task)
+        {
+            int taskId = task.Id;
+
+            DBTask tsk;
+
+            if (taskId < 1)
+            {
+                taskId = db.DBTasks
+                    .OrderBy(x => x.Id)
+                    .LastOrDefault()?.Id ?? 1;
+                taskId++;
+                task.Id = taskId;
+                tsk = task;
+                db.DBTasks.Add(task);
+            }
+            else
+            {
+                tsk = db.DBTasks
+                    .FirstOrDefault(x => x.Id == taskId);
+                if (tsk == null)
+                    throw new NullReferenceException($"Нет записи user с таким Id = {taskId}");
+                db.Entry(tsk).CurrentValues.SetValues(task);
+                db.Entry(tsk).State = EntityState.Modified;
+            }
+
+            return tsk;
+        }
+    }
+}
